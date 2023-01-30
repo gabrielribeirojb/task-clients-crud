80f792903dac4008013cd3fcdfefe548bc2bdac4
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2023-01-30 16:34:33 -0300

    Client dto and Client fix

--- src/main/java/com/devsuperior/entities/Client.java
+++ src/main/java/com/devsuperior/entities/Client.java
@@ -26,6 +26,19 @@ public class Client implements Serializable {
 	private Instant birthDate;
 	private Integer children;
 	
+	public Client() {
+		
+	}
+	
+	public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.income = income;
+		this.birthDate = birthDate;
+		this.children = children;
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -72,5 +85,30 @@ public class Client implements Serializable {
 	
 	public void setChildren(Integer children) {
 		this.children = children;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 }
