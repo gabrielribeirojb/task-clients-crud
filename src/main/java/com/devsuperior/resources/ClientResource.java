dfb75a841893b94051deb2352faf04aa264cd438
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2023-01-30 17:42:46 -0300

    POST Client

--- src/main/java/com/devsuperior/resources/ClientResource.java
+++ src/main/java/com/devsuperior/resources/ClientResource.java
@@ -1,5 +1,7 @@
 package com.devsuperior.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -7,9 +9,12 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dto.ClientDTO;
 import com.devsuperior.services.ClientService;
@@ -20,23 +25,30 @@ public class ClientResource {
 
 	@Autowired
 	private ClientService service;
-	
+
 	@GetMapping
-	public ResponseEntity<Page<ClientDTO>> findAllPaged(
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
+	public ResponseEntity<Page<ClientDTO>> findAllPaged(@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
 			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
-			@RequestParam(value = "direction", defaultValue = "ASC") String direction){
-		
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		Page<ClientDTO> list = service.findAllPaged(pageRequest);
-		
+
 		return ResponseEntity.ok().body(list);
 	};
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
 		ClientDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- src/main/java/com/devsuperior/services/ClientService.java
+++ src/main/java/com/devsuperior/services/ClientService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dto.ClientDTO;
 import com.devsuperior.entities.Client;
@@ -18,15 +19,33 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 
+	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
 		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(x -> new ClientDTO(x));
 	}
 	
+	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
 		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new ClientDTO(entity);
 	}
 	
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		copyEntityToDto(dto, entity);
+		
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+	
+	private void copyEntityToDto(ClientDTO dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+	}
 }
