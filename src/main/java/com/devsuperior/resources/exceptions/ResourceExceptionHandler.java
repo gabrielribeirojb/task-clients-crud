9bd8ab53878909941dfd0e536276c3d34eb53ce2
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2023-01-30 17:21:30 -0300

    findById and exceptions

---
+++ src/main/java/com/devsuperior/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.devsuperior.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(HttpServletRequest request, ResourceNotFoundException e){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError error = new StandardError();
+		error.setTimestamp(Instant.now());
+		error.setMessage(e.getMessage());
+		error.setStatus(status.value());
+		error.setError("Entity not found");
+		error.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(error);
+	}
+	
+}
--- src/main/java/com/devsuperior/services/ClientService.java
+++ src/main/java/com/devsuperior/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.dto.ClientDTO;
 import com.devsuperior.entities.Client;
 import com.devsuperior.repositories.ClientRepository;
+import com.devsuperior.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -18,6 +21,12 @@ public class ClientService {
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
 		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(x -> new ClientDTO(x));
+	}
+	
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
 	}
 	
 }
